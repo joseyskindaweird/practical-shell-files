814e5d26e42778dbde47aac9352f9015516eb586
Strange <84677793+strangetheproot@users.noreply.github.com>
2024-01-26 13:50:21 -0600

    RustyDingus
    Runs Rustdesk in correlation with the main file thats finna be here later

---
+++ RustDesk.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check for sudo privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run the script with sudo or as root."
+  exit 1
+fi
+
+# Download the .AppImage file
+wget -O RustDesk.AppImage https://github.com/rustdesk/rustdesk/releases/latest/download/rustdesk-1.2.3-x86_64.AppImage
+
+# Make the downloaded file executable
+chmod +x RustDesk.AppImage
+
+# Runs the thing.
+./RustDesk.AppImage
+
+# Permenant Solution
+wget -O RunMeForRustDesk.sh 
+
+# Gibs the choice
+read -p "Time to go back! Press y :3" choice
+
+if [ "$choice" == "y" ]; then
+  exit 1
+else
+  echo "tf you doing? wrong button bro. Either way, ur going back >:3"
+  exit 1
+fi
